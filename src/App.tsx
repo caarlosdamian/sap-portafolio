0f704d6fc5a861f83f3e422e3f886f50ee8a36a5
caarlosdamian <dapecarlos@gmail.com>
2022-12-29 23:32:31 -0600

    feat(app): adding some componentes and basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import './index.scss';
+import React from "react";
+import "./index.scss";
+import { Contact, Experience, Footer, Header, Hero } from "./sections";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app-container">
+      <Header />
+      <Hero />
+      <Experience />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/sections/index.ts
@@ -0,0 +1,6 @@
+export { Contact } from "./contact/Contact";
+export { Footer } from "./footer/Footer";
+export { Projects } from "./projects/Projects";
+export { Experience } from "./experience/Experience";
+export { Hero } from "./hero/Hero";
+export { Header } from "./header/Header";
