7ea441f2d29d153df0c6994014f8e1cbc5990d3a
caarlosdamian <dapecarlos@gmail.com>
2022-12-31 18:35:01 -0600

    feat:(hero): added photo

--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import AvatarImg from '../../assets/images/image-profile-mobile.webp'
+import './Hero.scss'
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero-container'>
+      <img src={AvatarImg} alt="AvatarImg"  className='hero-img'/>
+    </div>
   )
 }
