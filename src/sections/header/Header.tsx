0a55a36adc5341f852468d4f1a05af6657c1fd8d
caarlosdamian <dapecarlos@gmail.com>
2022-12-31 18:51:17 -0600

    feat(header): added header component styling mobile first

--- src/sections/header/Header.tsx
+++ src/sections/header/Header.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import FrontMentor from "../../assets/images/icon-frontend-mentor.svg";
+import Github from "../../assets/images/icon-github.svg";
+import Linkedin from "../../assets/images/icon-linkedin.svg";
+import Twitter from "../../assets/images/icon-twitter.svg";
+import Rings from "../../assets/images/pattern-rings.svg";
+import Circle from "../../assets/images/pattern-circle.svg";
+import "./Header.scss";
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header-container">
+      <img src={Circle} alt="" className="header-pattern-circle" />
+      <img src={Rings} alt="" className="header-pattern-rings" />
+      <h1 className="header-logo">adamkeyes</h1>
+      <div className="header-social-container">
+        <img src={Github} alt="" className="header-social-icon" />
+        <img src={FrontMentor} alt="" className="header-social-icon" />
+        <img src={Linkedin} alt="" className="header-social-icon" />
+        <img src={Twitter} alt="" className="header-social-icon" />
+      </div>
+    </div>
+  );
+};
--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import AvatarImg from '../../assets/images/image-profile-mobile.webp'
-import './Hero.scss'
+import React from "react";
+import AvatarImg from "../../assets/images/image-profile-mobile.webp";
+import "./Hero.scss";
 
 export const Hero = () => {
   return (
-    <div className='hero-container'>
-      <img src={AvatarImg} alt="AvatarImg"  className='hero-img'/>
+    <div className="hero-container">
+      <img src={AvatarImg} alt="AvatarImg" className="hero-img" />
     </div>
-  )
-}
+  );
+};
