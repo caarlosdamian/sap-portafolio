a90587b5926c03541e680f84aa245fe943b6e755
caarlosdamian <dapecarlos@gmail.com>
2023-01-04 23:28:59 -0600

    fix(header): added overflow hidden

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import "./index.scss";
+import Rings from "./assets/images/pattern-rings.svg";
+import Circle from "./assets/images/pattern-circle.svg";
 import { Contact, Experience, Footer, Header, Hero } from "./sections";
+import "./index.scss";
 
 function App() {
   return (
     <div className="app-container">
+      <img src={Circle} alt="" className="header-pattern-circle" />
+      <img src={Rings} alt="" className="header-pattern-rings" />
       <Header />
       <Hero />
       <Experience />
--- src/sections/header/Header.tsx
+++ src/sections/header/Header.tsx
@@ -3,15 +3,11 @@ import FrontMentor from "../../assets/images/icon-frontend-mentor.svg";
 import Github from "../../assets/images/icon-github.svg";
 import Linkedin from "../../assets/images/icon-linkedin.svg";
 import Twitter from "../../assets/images/icon-twitter.svg";
-import Rings from "../../assets/images/pattern-rings.svg";
-import Circle from "../../assets/images/pattern-circle.svg";
 import "./Header.scss";
 
 export const Header = () => {
   return (
     <div className="header-container">
-      <img src={Circle} alt="" className="header-pattern-circle" />
-      <img src={Rings} alt="" className="header-pattern-rings" />
       <h1 className="header-logo">adamkeyes</h1>
       <div className="header-social-container">
         <img src={Github} alt="" className="header-social-icon" />
