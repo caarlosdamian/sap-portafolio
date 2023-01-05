a934072f885ee95569ad6e6195db29a019de9be2
caarlosdamian <dapecarlos@gmail.com>
2023-01-04 23:39:08 -0600

    feat(hero): added hero section

--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -6,6 +6,14 @@ export const Hero = () => {
   return (
     <div className="hero-container">
       <img src={AvatarImg} alt="AvatarImg" className="hero-img" />
+      <h1 className="hero-title">
+        Nice to meet you! I’m <strong className="hero-name">Adam Keyes</strong>.
+      </h1>
+      <p className="hero-description">
+        Based in the UK, I’m a front-end developer passionate about building
+        accessible web apps that users love.
+      </p>
+      <button className="hero-button">CONTACT ME</button>
     </div>
   );
 };
