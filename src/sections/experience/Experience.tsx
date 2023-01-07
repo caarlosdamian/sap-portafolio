454cbf5e773c05f99e87d822decc99f3535b0243
caarlosdamian <dapecarlos@gmail.com>
2023-01-06 23:16:57 -0600

    feat(experience): added experience section

--- src/sections/experience/Experience.tsx
+++ src/sections/experience/Experience.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import { experienceData } from "../../utils";
+import "./Experience.scss";
 
 export const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <div className="experience-container">
+      {experienceData.map((item) => (
+        <div className="experience-labels-container" key={item.id}>
+          <h1 className="experience-title">{item.title}</h1>
+          <span className="experience-description">{item.description}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
---
+++ src/utils/index.ts
@@ -0,0 +1,32 @@
+export const experienceData = [
+  {
+    id: 1,
+    title: "HTML",
+    description: "4 Years Experience",
+  },
+  {
+    id: 2,
+    title: "CSS",
+    description: "4 Years Experience",
+  },
+  {
+    id: 3,
+    title: "Javascript",
+    description: "4 Years Experience",
+  },
+  {
+    id: 4,
+    title: "Accessibility",
+    description: "4 Years Experience",
+  },
+  {
+    id: 5,
+    title: "React",
+    description: "3 Years Experience",
+  },
+  {
+    id: 6,
+    title: "Sass",
+    description: "3 Years Experience",
+  },
+];
