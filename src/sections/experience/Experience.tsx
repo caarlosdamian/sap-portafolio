c892770a7513a28b8b651dec36cdb7bf0eb17034
caarlosdamian <dapecarlos@gmail.com>
2023-01-06 23:17:16 -0600

    feat(experience): added experience section

--- src/sections/experience/Experience.tsx
+++ src/sections/experience/Experience.tsx
@@ -4,13 +4,13 @@ import "./Experience.scss";
 
 export const Experience = () => {
   return (
-    <div className="experience-container">
+    <section className="experience-container">
       {experienceData.map((item) => (
         <div className="experience-labels-container" key={item.id}>
           <h1 className="experience-title">{item.title}</h1>
           <span className="experience-description">{item.description}</span>
         </div>
       ))}
-    </div>
+    </section>
   );
 };
