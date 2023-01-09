c8dbbcbaa429f90ca89b2aded08866ed484c1fb4
caarlosdamian <dapecarlos@gmail.com>
2023-01-08 22:19:18 -0600

    refactor(experience): added missing img

--- src/sections/experience/Experience.tsx
+++ src/sections/experience/Experience.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { experienceData } from "../../utils";
+import Rings from "../../assets/images/pattern-rings.svg";
+
 import "./Experience.scss";
 
 export const Experience = () => {
   return (
     <section className="experience-container">
+      <img
+        src={Rings}
+        alt="rings-bottom"
+        className="header-pattern-rings-experience"
+      />
       {experienceData.map((item) => (
         <div className="experience-labels-container" key={item.id}>
           <h1 className="experience-title">{item.title}</h1>
